d01201c99f9b7eb5eec10573071da9647e597dd2
elenaroncevic <roncevicelena@gmail.com>
2018-05-22 18:19:15 +0200

    i ovo samo jos

--- project/src/main/java/app/repository/SeatRepository.java
+++ project/src/main/java/app/repository/SeatRepository.java
@@ -1,6 +1,5 @@
 package app.repository;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,12 +10,7 @@ public interface SeatRepository extends JpaRepository<Seat, Long> {
 	List<Seat> findByNumber(String number);
 
 	List<Seat> findByNumberBetween(String number1, String number2);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import app.model.Seat;
 
-public interface SeatRepository extends JpaRepository<Seat, Long> {
 
->>>>>>> branch 'master' of https://github.com/elenaroncevic/mrs8
 }
